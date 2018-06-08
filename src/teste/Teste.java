31d4813d56cfe74035901fe8172682946b53a94c
laboratorio <laboratorio@laboratorioA5.labifms.edu.br>
2018-06-07 13:23:27 -1200

    segundo commit

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -16,7 +16,7 @@ public class Teste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ola mundo");
+        System.out.println("ola mundo novo");
         System.out.println("semana do meio ambiente");
        
         
